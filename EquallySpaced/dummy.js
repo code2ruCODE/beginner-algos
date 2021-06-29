73e524b49ed2a74cedb12d6d6baf30f3f764f407
petrepan <peterawotola@gmail.com>
2021-06-29 08:02:27 +0100

    first commit

---
+++ EquallySpaced/dummy.js
@@ -0,0 +1,20 @@
+function evenlySpaced(first, second, last) {
+  //Provide your solution here
+
+  let numberArray = [];
+  numberArray.push(first);
+  numberArray.push(second);
+  numberArray.push(last);
+
+  numberArray.sort(function (a, b) {
+    return a - b;
+  });
+
+  if (numberArray[1] - numberArray[0] !== numberArray[2] - numberArray[1]) {
+    return false;
+  }
+
+  return true;
+}
+
+evenlySpaced(4, 6, 3);
